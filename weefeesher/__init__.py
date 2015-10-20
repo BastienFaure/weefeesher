27c515863f618c8cb1c9ff9ddf5ec1199b2955ef
Bastien Faure <bastien@faure.io>
2015-10-20 14:17:09 +0200

    added executable arg

--- weefeesher/__init__.py
+++ weefeesher/__init__.py
@@ -25,8 +25,8 @@ def flush_iptables():
     subprocess.call('iptables -X', shell=True)
 
 def restore_iptables():
-    subprocess.call('iptables-restore < /tmp/weefeesher.iptables.state', shell=True)
-    subprocess.call('rm -f /tmp/weefeesher.iptables.state', shell=True)
+    subprocess.call('iptables-restore < /tmp/weefeesher.iptables.state', shell=True, executable="/bin/bash")
+    subprocess.call('rm -f /tmp/weefeesher.iptables.state', shell=True, executable="/bin/bash")
 
 class Web():
 
@@ -56,31 +56,31 @@ class Dnsmasq():
         pass
 
     def backup(self):
-        subprocess.call('[[ -f /etc/dnsmasq.conf ]] && mv /etc/dnsmasq.conf /tmp/weefeesher.dnsmasq.conf', shell=True)
+        subprocess.call('[[ -f /etc/dnsmasq.conf ]] && mv /etc/dnsmasq.conf /tmp/weefeesher.dnsmasq.conf', shell=True, executable="/bin/bash")
 
     def deploy(self):
-        subprocess.call('cp %s/conf/dnsmasq.conf /etc/dnsmasq.conf' % here, shell=True)
-        subprocess.call('killall -9 dnsmasq &> /dev/null; dnsmasq', shell=True)
+        subprocess.call('cp %s/conf/dnsmasq.conf /etc/dnsmasq.conf' % here, shell=True, executable="/bin/bash")
+        subprocess.call('killall -9 dnsmasq &> /dev/null; dnsmasq', shell=True, executable="/bin/bash")
 
     def restore(self):
     	Logger.info('Restoring dnsmasq base config file')
-        subprocess.call('[[ -f /tmp/weefeesher.dnsmasq.conf ]] && mv /tmp/weefeesher.dnsmasq.conf /etc/dnsmasq.conf', shell=True)
+        subprocess.call('[[ -f /tmp/weefeesher.dnsmasq.conf ]] && mv /tmp/weefeesher.dnsmasq.conf /etc/dnsmasq.conf', shell=True, executable="/bin/bash")
 
     def kill(self):
     	Logger.info('Killing dnsmasq instance')
-        subprocess.call('killall -9 dnsmasq &> /dev/null', shell=True)
+        subprocess.call('killall -9 dnsmasq &> /dev/null', shell=True, executable="/bin/bash")
 
 def check_requirements():
 
-    user = subprocess.check_output('whoami', shell=True)[:-1]
+    user = subprocess.check_output('whoami', shell=True, executable="/bin/bash")[:-1]
     if user != 'root':
         Logger.error("Please launch this program as root")
         sys.exit(0)
-    dnsmasq = subprocess.call('which dnsmasq &> /dev/null', shell=True)
+    dnsmasq = subprocess.call('which dnsmasq &> /dev/null', shell=True, executable="/bin/bash")
     if dnsmasq:
         Logger.error("Please install dnsmasq")
         sys.exit(0)
-    airbase = subprocess.call('which airbase-ng &> /dev/null', shell=True)
+    airbase = subprocess.call('which airbase-ng &> /dev/null', shell=True, executable="/bin/bash")
     if airbase:
         Logger.error("Please install aircrack suite, especially airbase-ng")
         sys.exit(0)
@@ -91,9 +91,9 @@ def signal_handler(signal, frame):
     Logger.info('Shutting down rogue AP')
     subprocess.call('pkill -9 airbase-ng', shell=True)
     Logger.info('Up wireless interface')
-    subprocess.call('ip link set %s up' % phishing_iface, shell=True)
-    subprocess.call('ip link set %s up' % phishing_iface, shell=True)
-    subprocess.call('ip link set %s up' % phishing_iface, shell=True)
+    subprocess.call('ip link set %s up' % phishing_iface, shell=True, executable="/bin/bash")
+    subprocess.call('ip link set %s up' % phishing_iface, shell=True, executable="/bin/bash")
+    subprocess.call('ip link set %s up' % phishing_iface, shell=True, executable="/bin/bash")
     Logger.info('Restoring iptables rules')
     restore_iptables()
     dns.kill()
@@ -103,19 +103,19 @@ def signal_handler(signal, frame):
 
 def launch_ap(iface, essid):
     Logger.info('Shutting down phishing interface %s' % iface)
-    subprocess.call('ip link set %s down' % iface, shell=True)
-    subprocess.call('ip link set %s down' % iface, shell=True)
-    subprocess.call('ip link set %s down' % iface, shell=True)
-    subprocess.call('iw %s set type monitor' % iface, shell=True)
-    subprocess.call('iw %s set type monitor' % iface, shell=True)
-    subprocess.call('iw %s set type monitor' % iface, shell=True)
-    subprocess.call('ip link set %s up' % iface, shell=True)
-    subprocess.call('ip link set %s up' % iface, shell=True)
-    subprocess.call('ip link set %s up' % iface, shell=True)
+    subprocess.call('ip link set %s down' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('ip link set %s down' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('ip link set %s down' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('iw %s set type monitor' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('iw %s set type monitor' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('iw %s set type monitor' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('ip link set %s up' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('ip link set %s up' % iface, shell=True, executable="/bin/bash")
+    subprocess.call('ip link set %s up' % iface, shell=True, executable="/bin/bash")
     Logger.info('Launching rogue access point...')
-    subprocess.Popen('airbase-ng --essid %s -I 60 %s &> /dev/null' % (essid, iface), shell=True)
+    subprocess.Popen('airbase-ng --essid %s -I 60 %s &> /dev/null' % (essid, iface), shell=True, executable="/bin/bash")
     time.sleep(2)
-    subprocess.call('ip addr add 192.168.10.1/24 dev at0', shell=True)
+    subprocess.call('ip addr add 192.168.10.1/24 dev at0', shell=True, executable="/bin/bash")
 
 
 def main():
